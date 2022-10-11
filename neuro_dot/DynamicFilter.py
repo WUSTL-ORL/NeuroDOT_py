4440f472d40685f8e21805863a96049a7e9f6e70
emmaspeh <espeh@wustl.edu>
2022-10-11 10:49:40 -0500

    Updated getting_started.ipynb and DynamicFilter.py

--- neuro_dot/DynamicFilter.py
+++ neuro_dot/DynamicFilter.py
@@ -1,7 +1,7 @@
 # Import all necessary Python modules
 import numpy as np 
 import scipy.io as sio
-import sys  
+import sys,os  
 import matplotlib.pyplot as plt  
 import math
 import numpy.matlib as nm
@@ -11,20 +11,15 @@ import matplotlib.gridspec as gridspec
 from matplotlib.cm import jet
 import copy
 # Add all relevant paths (e.g. folders containing NeuroDOT functions and data)
-sys.path.insert(0,'E:/Emma/NURO_DOT_dev/Individual_Functions/')
-sys.path.insert(0,'E:/Emma/NURO_DOT_dev/Matlab_Equivalent_Functions/')
-sys.path.insert(0,'E:/Emma/NURO_DOT_dev/Individual_Functions/Analysis/')
-sys.path.insert(0,'E:/Emma/NURO_DOT_dev/Individual_Functions/File_IO/')
-sys.path.insert(0,'E:/Emma/NURO_DOT_dev/Individual_Functions/Temporal_Transforms/')
-sys.path.insert(0,'E:/Emma/NURO_DOT_dev/Individual_Functions/Visualizations/')
-sys.path.insert(0, '/jupyter/nuro_dot/NURO_DOT_dev/')
-sys.path.insert(0,'E:/python_codes/NeuroDOT_dev/Data')
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'neuro_dot'))
+
+
 
 # Import all functions that will be used in the pipeline (consider using __init__ file instead)
 # In __init__ file in 'Individual Functions' directory, change the path to your system folders
 # containing each function folder
 
-sys.path.insert(0,'E:/Emma/NURO_DOT_dev/neuro_dot')
+
 from Visualizations import viz
 from Spatial_Transforms import sx4m
 from Temporal_Transforms import tx4m
